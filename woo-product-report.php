d1f2ecf5ada7d08195cb9ca645cac061323b7319
Skian Mhor test <skianmhor@3410-5211-4975.bhost.net>
2019-10-26 13:18:33 +0100

    fleshing it out

--- woo-product-report.php
+++ woo-product-report.php
@@ -11,6 +11,7 @@
 class Woo_Product_Report {
     public static function setup() {
         add_action('admin_menu', [self::class, 'admin_menu']);
+        add_action('admin_enqueue_scripts', [self::class, 'admin_scripts']);
     }
     
     public static function admin_menu() {
@@ -25,8 +26,48 @@ class Woo_Product_Report {
     }
     
     public static function admin_page() {
+        global $plugin_page;
         $products = array_map([self::class, 'get_product_option'], self::get_products());
-        echo '<select>' . $products . '</select>';
+?>
+<form action="" method="get">
+<input name="page" type="hidden" value="<?php echo $plugin_page; ?>"/>
+<label for="product">Select a product</label>
+<select id="product" name="product"><option/><?php echo implode('', $products); ?></select>
+<button type="submit">View</button>
+</form>
+<script type="text/javascript">jQuery('#product').selectWoo();</script>
+<hr/>
+<table class="wp-list-table widefat striped">
+<thead>
+    <tr><th>Date</th><th>Status</th><th>Name</th><th>Qty</th><th>Notes</th></tr>
+</thead>
+<tbody>
+<?php
+$orders = self::get_orders_from_product($_REQUEST['product'] ?? '');
+if (count($orders)): ?>
+<?php else: ?>
+<tr><td colspan="5">No orders for this product</td></tr>
+<?php endif; ?>
+</tbody>
+</table>
+<?php
+    }
+
+    public static function admin_scripts() {
+        global $plugin_page;
+        if ('woo_product_report' == $plugin_page) {
+            wp_register_style( 'woocommerce_admin_styles', WC()->plugin_url() . '/assets/css/admin.css', array(), WC_VERSION );
+            wp_enqueue_style('woocommerce_admin_styles');
+
+            wp_register_script('select2', WC()->plugin_url() . '/assets/js/select2/select2.full.min.js', array('jquery'));
+            wp_enqueue_script('select2');
+
+            wp_register_script('selectWoo', WC()->plugin_url() . '/assets/js/selectWoo/selectWoo.full.min.js', array('jquery'));
+            wp_enqueue_script('selectWoo');
+
+            wp_register_script('wc-enhanced-select', WC()->plugin_url() . '/assets/js/admin/wc-enhanced-select.min.js', array('jquery', 'selectWoo'));
+            wp_enqueue_script('wc-enhanced-select');
+        }
     }
     
     public static function get_products() {
@@ -36,7 +77,14 @@ class Woo_Product_Report {
     }
     
     public static function get_product_option($product) {
-        return '<option value="' . $product->getId() . '">' . $product->getName() . '(#' . $product->getId() . ')</option>';
+        $selected = !empty($_REQUEST['product']) && $product->get_id() == $_REQUEST['product'] ? ' selected="selected"' : '';
+        return '<option value="' . $product->get_id() . '"' . $selected . '>' . $product->get_name() . '(#' . $product->get_id() . ')</option>';
+    }
+
+    public static function get_orders_from_product($product_id = null) {
+        if ($product_id) {
+        }
+        return [];
     }
 }
  
